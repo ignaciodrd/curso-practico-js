c01cda50ec633f46d7ee628dbc0712b66599c4ba
ignaciodrd <ignaciodrd@gmail.com>
2021-08-24 15:19:09 -0400

    cuarto taller

---
+++ taller4/salarios.js
@@ -0,0 +1,78 @@
+const colombia = [];
+
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Pedro",
+    salary: 1200,
+});
+colombia.push({
+    name: "Martin",
+    salary: 500,
+});
+colombia.push({
+    name: "Rosa",
+    salary: 10000,
+});
+colombia.push({
+    name: "Gloria",
+    salary: 2000,
+});
+colombia.push({
+    name: "Ignacio",
+    salary: 5000,
+});
+colombia.push({
+    name: "Juan",
+    salary: 8000,
+});
+colombia.push({
+    name: "Maria",
+    salary: 7000,
+});
+colombia.push({
+    name: "Jose",
+    salary: 1000,
+});
+colombia.push({
+    name: "Manuel",
+    salary: 1000,
+});
+colombia.push({
+    name: "Andrea",
+    salary: 1500,
+});
+colombia.push({
+    name: "Alvaro",
+    salary: 2500,
+});
+colombia.push({
+    name: "Marta",
+    salary: 3000,
+});
+colombia.push({
+    name: "Mirta",
+    salary: 900,
+});
+colombia.push({
+    name: "Seba",
+    salary: 1600,
+});
+colombia.push({
+    name: "Javier",
+    salary: 3000,
+});
+colombia.push({
+    name: "Rodrigo",
+    salary: 1800,
+});
+colombia.push({
+    name: "Gustavo",
+    salary: 2100,
+});
+colombia.push({
+    name: "Miriam",
+    salary: 2500,
+});
